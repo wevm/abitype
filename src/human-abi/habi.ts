d62683da8f48fbba2c85b6e73f32cd68a6e25c41
Raiden1411 <joao9_bernardino@hotmail.com>
2023-01-30 17:29:29 +0000

    Adds proper documentation

--- src/human-abi/habi.ts
+++ src/human-abi/habi.ts
@@ -12,15 +12,45 @@ import type {
   isTupleValue,
 } from './utils'
 
+/**
+ * Extract type from HAbi string.
+ *
+ * @param T - Any string
+ * @returns "function" | "event" | "error"  in case it matches or never if it doesn't
+ *
+ * @example
+ * type Result = ExtractType<"function hello(string world)">
+ * //    ^? "function"
+ */
 export type ExtractType<T> = T extends `${infer TType}${WS}${string}`
   ? TType extends AbiTypes
     ? TType
     : never
   : never
 
+/**
+ * Extract name from HAbi string.
+ *
+ * @param T - Assumes the string starts with `"function" | "event" | "error"` followed by `WS` in order to extract the name
+ * @returns The name in case of a match or never in case it fails
+ *
+ * @example
+ * type Result = ExtractName<"function hello(string world)">
+ * //    ^? "hello"
+ */
 export type ExtractNames<T extends string> =
   T extends `${AbiTypes}${WS}${infer TName}(${string})${string}` ? TName : never
 
+/**
+ * Extract mutabiliy from HAbi string.
+ *
+ * @param T - String to extract values from
+ * @returns Value extracted from the string in case of a match or 'nonpayable' in case it didn't
+ *
+ * @example
+ * type Result = ExtractMutability<"function hello(string world) view returns(uint token)">
+ * //    ^? "view"
+ */
 export type ExtractMutability<T extends string> =
   T extends `${AbiTypes}${WS}${string}(${string})${WS}${infer P}${WS}${string}`
     ? P extends AbiMutability
@@ -28,11 +58,31 @@ export type ExtractMutability<T extends string> =
       : 'nonpayable'
     : 'nonpayable'
 
+/**
+ * Extract return values from HAbi string.
+ *
+ * @param T - String in the HAbi format
+ * @returns The value inside of the `()`
+ *
+ * @example
+ * type Result = ExtractReturn<"function hello(string world) view returns (uint token)">
+ * //    ^? "uint token"
+ */
 export type ExtractReturn<T extends string> =
   T extends `${string}returns (${infer TName})`
     ? ReOrderArray<SplitNesting<ReplaceAll<TName, 'tuple', ''>>>
     : ['void']
 
+/**
+ * Extract the arguments from HAbi string.
+ *
+ * @param T - String in the HAbi format. All tuple values in the string are replaced
+ * @returns A re-orded array with the values extracted from the string
+ *
+ * @example
+ * type Result = ExtractArgs<"function hello(string world)">
+ * //    ^? ["string world"]
+ */
 export type ExtractArgs<T extends string> = hasTupleValue<T> extends true
   ? T extends `${AbiTypes}${WS}${string}(${infer TName})${WS}${string}returns${string}`
     ? ReOrderArray<SplitNesting<ReplaceAll<TName, 'tuple', ''>>>
@@ -45,34 +95,29 @@ export type ExtractArgs<T extends string> = hasTupleValue<T> extends true
   ? ReOrderArray<SplitNesting<ReplaceAll<TNames, 'tuple', ''>>>
   : never
 
-// export type ExtractTupleName<
-//   T extends string,
-//   TRemove = Remove<T, ExtractTArgs<T>>,
-// > = TRemove extends `tuple()[${string}]${WS | AbiIndexed}${infer Name}`
-//   ? Trim<Name>
-//   : TRemove extends `tuple()${WS | AbiIndexed}${infer Name}`
-//   ? Trim<Name>
-//   : ''
-
-// export type ExtractTupleType<T extends string> = Remove<
-//   T,
-//   ExtractTArgs<T>
-// > extends `tuple()[${infer K}]${string}`
-//   ? `tuple[${K}]`
-//   : 'tuple'
-
-// export type ExtractTupleInternalType<
-//   T extends string,
-//   TRemove = Remove<T, ExtractTArgs<T>>,
-// > = TRemove extends `tuple()[${infer K}]${WS | AbiIndexed}${infer Name}`
-//   ? `Struct[${K}]${WS}${Name}`
-//   : TRemove extends `tuple()${WS | AbiIndexed}${infer Name}`
-//   ? `Struct${WS}${Name}`
-//   : 'Struct'
-
+/**
+ * Check if the string has `indexed` value in it.
+ *
+ * @param T - Target string to check
+ * @returns True in case of a match. False if it doesn't
+ *
+ * @example
+ * type Result = isIndexed<"function hello(string world)">
+ * //    ^? "false"
+ */
 export type isIndexed<T extends string> =
   T extends `${string}${AbiIndexed}${string}` ? true : unknown
 
+/**
+ * Extract all the info related to strings that end in ")". Does this recursively in case of nested values until it no longer matches.
+ *
+ * @param T - Target string to check
+ * @returns The extracted trimmed values or a empty string in case it didn't match
+ *
+ * @example
+ * type Result = ExtractTupleInfo<"((string world)[] worlds, address hello) person">
+ * //    ^? "person"
+ */
 export type ExtractTupleInfo<T extends string> =
   T extends `${string})${infer Info}`
     ? Info extends `${string})`
@@ -83,6 +128,17 @@ export type ExtractTupleInfo<T extends string> =
         : ExtractTupleInfo<Trim<TInfo>>
       : Trim<Info>
     : T
+
+/**
+ * Extract the tuple arguments.
+ *
+ * @param T - String to extract values from
+ * @returns The values in case it matches or a empty string in case it didn't
+ *
+ * @example
+ * type Result = ExtractTArgs<"(string world)[] person">
+ * //    ^? "string world"
+ */
 export type ExtractTArgs<T extends string> =
   T extends `(${infer Args})${ExtractTupleInfo<T>}`
     ? Args
@@ -90,6 +146,16 @@ export type ExtractTArgs<T extends string> =
     ? Args
     : ''
 
+/**
+ * Extract name from tuple string.
+ *
+ * @param T - String to extract values from
+ * @returns The name of the tuple in case it has any
+ *
+ * @example
+ * type Result = ExtractTupleName<"(string world)[] person">
+ * //    ^? "person"
+ */
 export type ExtractTupleName<T extends string> =
   T extends `[${string}]${AbiIndexed}${infer Name}`
     ? Name
@@ -99,9 +165,29 @@ export type ExtractTupleName<T extends string> =
     ? Name
     : T
 
+/**
+ * Extract type from tuple string.
+ *
+ * @param T - String to extract values from
+ * @returns the tuple type depending on {@link T}
+ *
+ * @example
+ * type Result = ExtractTupleType<"(string world)[3] person">
+ * //    ^? "tuple[3]"
+ */
 export type ExtractTupleType<T extends string> =
   T extends `[${infer K}]${string}` ? `tuple[${K}]` : 'tuple'
 
+/**
+ * Extract internal type from tuple string.
+ *
+ * @param T - String to extract values from
+ * @returns the tuple internal type depending on {@link T}
+ *
+ * @example
+ * type Result = ExtractTupleType<"(string world)[3] person">
+ * //    ^? "Struct[3] person"
+ */
 export type ExtractTupleInternalType<T extends string> =
   T extends `[${infer K}]${AbiIndexed}${infer Name}`
     ? `Struct[${K}]${WS}${Name}`
@@ -111,31 +197,16 @@ export type ExtractTupleInternalType<T extends string> =
     ? `Struct${WS}${Name}`
     : Trim<`Struct${WS}${T}`>
 
-// export type ExtractTArgs<T extends string> =
-//   T extends `tuple(${infer A})${infer B})${infer C})${infer D})${infer E})${infer F})${infer G})${infer H})${infer I})${infer J})${string}`
-//     ? `${A})${B})${C})${D})${E})${F})${G})${H})${I})${J}`
-//     : T extends `tuple(${infer A})${infer B})${infer C})${infer D})${infer E})${infer F})${infer G})${infer H})${infer I})${string}`
-//     ? `${A})${B})${C})${D})${E})${F})${G})${H})${I}`
-//     : T extends `tuple(${infer A})${infer B})${infer C})${infer D})${infer E})${infer F})${infer G})${infer H})${string}`
-//     ? `${A})${B})${C})${D})${E})${F})${G})${H}`
-//     : T extends `tuple(${infer A})${infer B})${infer C})${infer D})${infer E})${infer F})${infer G})${string}`
-//     ? `${A})${B})${C})${D})${E})${F})${G}`
-//     : T extends `tuple(${infer A})${infer B})${infer C})${infer D})${infer E})${infer F})${string}`
-//     ? `${A})${B})${C})${D})${E})${F}`
-//     : T extends `tuple(${infer A})${infer B})${infer C})${infer D})${infer E})${string}`
-//     ? `${A})${B})${C})${D})${E}`
-//     : T extends `tuple(${infer A})${infer B})${infer C})${infer D})${string}`
-//     ? `${A})${B})${C})${D}`
-//     : T extends `tuple(${infer A})${infer B})${infer C})${string}`
-//     ? `${A})${B})${C}`
-//     : T extends `tuple(${infer A})${infer B})${string}`
-//     ? `${A})${B}`
-//     : T extends `tuple(${infer Args})[${string}]${string}`
-//     ? Args
-//     : T extends `tuple(${infer Args})${string}`
-//     ? Args
-//     : never
-
+/**
+ * Parse HAbi string arguments.
+ *
+ * @param T - String to parse
+ * @returns Array with the parsed abi values. Assumes values were extracted previously from {@link ExtractArgs}
+ *
+ * @example
+ * type Result = ParseFunctionArgs<["string world"]>
+ * //    ^? "[{name: "hello", type: "string", internalType: "string"}]"
+ */
 export type ParseFunctionArgs<T extends string[]> = T extends [never]
   ? never
   : T extends ['']
@@ -163,6 +234,16 @@ export type ParseFunctionArgs<T extends string[]> = T extends [never]
     ]
   : []
 
+/**
+ * Parse HAbi string return.
+ *
+ * @param T - String to parse
+ * @returns Array with the parsed abi values. Assumes values were extracted previously from {@link ExtractReturn}
+ *
+ * @example
+ * type Result = ParseFunctionReturn<["string world"]>
+ * //    ^? "[{name: "hello", type: "string", internalType: "string"}]"
+ */
 export type ParseFunctionReturn<T extends string[]> = T extends [never]
   ? never
   : T extends ['void']
@@ -190,6 +271,16 @@ export type ParseFunctionReturn<T extends string[]> = T extends [never]
     ]
   : []
 
+/**
+ * Parse HAbi event string arguments.
+ *
+ * @param T - String to parse
+ * @returns Array with the parsed abi values. Assumes values were extracted previously from {@link ExtractArgs}
+ *
+ * @example
+ * type Result = ParseFunctionArgs<["address indexed owner"]>
+ * //    ^? "[{name: "owner", type: "address", internalType: "address", indexed: true}]"
+ */
 export type ParseEventArgs<T extends string[]> = T extends [never]
   ? never
   : T extends ['']
@@ -221,6 +312,16 @@ export type ParseEventArgs<T extends string[]> = T extends [never]
     ]
   : []
 
+/**
+ * Parse HAbi tuple string arguments.
+ *
+ * @param T - String to parse
+ * @returns Array with the parsed abi values. Assumes values were extracted previously from {@link ExtractTArgs}
+ *
+ * @example
+ * type Result = ParseComponents<["(string world)[] person"]>
+ * //    ^? [{name: "person", type: "tuple[]", internalType: "Struct[] person", components:[{name: "world", type: "string", internalType: "world"}]}]
+ */
 export type ParseComponents<T extends unknown[]> = T extends [never]
   ? never
   : T extends ['']
@@ -265,6 +366,16 @@ export type ParseComponents<T extends unknown[]> = T extends [never]
     : []
   : []
 
+/**
+ * Parse HAbi tuple string arguments. This handles the indexed values on the string
+ *
+ * @param T - String to parse
+ * @returns Array with the parsed abi values. Assumes values were extracted previously from {@link ExtractTArgs}
+ *
+ * @example
+ * type Result = ParseComponents<["(string world)[] person"]>
+ * //    ^? [{name: "person", type: "tuple[]", internalType: "Struct[] person", indexed: true, components:[{name: "world", type: "string", internalType: "world"}]}]
+ */
 export type ParseEventComponents<T extends unknown[]> = T extends [never]
   ? never
   : T extends ['']
@@ -310,6 +421,16 @@ export type ParseEventComponents<T extends unknown[]> = T extends [never]
     : []
   : []
 
+/**
+ * Handle arguments based on if they are tuple values or not. Calls correct function accordingly
+ *
+ * @param T - String to parse
+ * @returns Array with the parsed abi values. Assumes values were extracted previously from {@link ExtractArgs}
+ *
+ * @example
+ * type Result = HandleArguments<["(string world)[] person"], "function">
+ * //    ^? [{name: "person", type: "tuple[]", internalType: "Struct[] person", indexed: true, components:[{name: "world", type: "string", internalType: "world"}]}]
+ */
 export type HandleArguments<
   T extends unknown[],
   TType extends 'function' | 'event',
@@ -325,6 +446,16 @@ export type HandleArguments<
     : [...ParseEventArgs<[THead]>, ...HandleArguments<Rest, 'event'>]
   : []
 
+/**
+ * Extract name from HAbi string.
+ *
+ * @param T - Assumes the string starts with `"function" | "event" | "error"` followed by `WS` in order to extract the name
+ * @returns The name in case of a match or never in case it fails
+ *
+ * @example
+ * type Result = ExtractName<"function hello(string world)">
+ * //    ^? "hello"
+ */
 export type HandleReturnArguments<T extends unknown[]> = T extends [
   infer THead extends string,
   ...infer Rest extends string[],
@@ -334,6 +465,16 @@ export type HandleReturnArguments<T extends unknown[]> = T extends [
     : [...ParseFunctionReturn<[THead]>, ...HandleReturnArguments<Rest>]
   : []
 
+/**
+ * Parses the HAbi type string into to proper abi representation. This one only works for function type strings
+ *
+ * @param T - String to parse. Must meet HAbi specs
+ * @returns Array with the parsed abi values.
+ *
+ * @example
+ * type Result = ParseHAbiFunctions<["function hello(string world)"]>
+ * //    ^? [{name: "hello", type: "function", constant: false, payable: false, inputs:[{name: "world", type: "string", internalType: "string"}], outputs:[]}]
+ */
 export type ParseHAbiFunctions<T extends HAbi> = T extends [never]
   ? never
   : T extends ''
@@ -363,6 +504,16 @@ export type ParseHAbiFunctions<T extends HAbi> = T extends [never]
     : ParseHAbiFunctions<Rest>
   : []
 
+/**
+ * Parses the HAbi type string into to proper abi representation. This one only works for event type strings
+ *
+ * @param T - String to parse. Must meet HAbi specs
+ * @returns Array with the parsed abi values.
+ *
+ * @example
+ * type Result = ParseHAbiEvents<["event hello(string world)"]>
+ * //    ^? [{name: "hello", type: "event", anonymous: false, inputs:[{name: "world", type: "string", internalType: "string"}]}]
+ */
 export type ParseHAbiEvents<T extends HAbi> = T extends [never]
   ? never
   : T extends ['']
@@ -383,6 +534,16 @@ export type ParseHAbiEvents<T extends HAbi> = T extends [never]
     : ParseHAbiEvents<Rest>
   : []
 
+/**
+ * Parses the HAbi type string into to proper abi representation. This one only works for error type strings
+ *
+ * @param T - String to parse. Must meet HAbi specs
+ * @returns Array with the parsed abi values.
+ *
+ * @example
+ * type Result = ParseHAbiErrors<["error hello(string world)"]>
+ * //    ^? [{name: "hello", type: "error", inputs:[{name: "world", type: "string", internalType: "string"}]}]
+ */
 export type ParseHAbiErrors<T extends HAbi> = T extends [never]
   ? never
   : T extends ['']
@@ -400,16 +561,39 @@ export type ParseHAbiErrors<T extends HAbi> = T extends [never]
     : ParseHAbiErrors<Rest>
   : []
 
+/**
+ * Parses all of the HAbi type string in to array of objects much like their abi representation
+ *
+ * @param HumanAbi - Array of strings that must meet HAbi specs
+ * @returns Array of the parsed strings
+ *
+ * @example
+ * type Result = ParseHumanAbi<["error hello(string world)","event hello(string world)","function hello(string world)"]>
+ * //    ^? [{name: "hello", type: "error", inputs:[{name: "world", type: "string", internalType: "string"}]},{name: "hello", type: "event", anonymous: false, inputs:[{name: "world", type: "string", internalType: "string"}]},{name: "hello", type: "function", constant: false, payable: false, inputs:[{name: "world", type: "string", internalType: "string"}], outputs:[]}]
+ */
 export type ParseHumanAbi<HumanAbi extends HAbi> = readonly [
   ...ParseHAbiErrors<HumanAbi>,
   ...ParseHAbiEvents<HumanAbi>,
   ...ParseHAbiFunctions<HumanAbi>,
 ]
 
+/**
+ * HAbi spec in order to properly parse the given array of strings
+ */
 export type HAbi = readonly (
   | `function${WS}${string}(${string})${WS}${AbiMutability}${WS}returns${WS}(${string})`
   | `function${WS}${string}(${string})${WS}returns${WS}(${string})`
   | `${AbiTypes}${WS}${string}(${string})`
 )[]
 
+/**
+ * Checks if an array of strings meets the HAbi specs
+ *
+ * @param THAbi - Array of strings to check
+ * @returns True if it matches or false it if doesn't
+ *
+ * @example
+ * type Result = IsHAbi<["functions hello(string world)"]>
+ * //    ^? "false"
+ */
 export type IsHAbi<THAbi> = THAbi extends HAbi ? true : false
--- src/human-abi/utils.ts
+++ src/human-abi/utils.ts
@@ -1,14 +1,47 @@
 export type WS = ' '
+
+/**
+ * Trim all whitespaces on the right side of a string
+ *
+ * @param S - String to trim
+ * @returns Trimmed string to the right side {@link S}
+ *
+ * @example
+ * type Result = TrimLeft<"Hello     ">
+ * //    ^? "Hello"
+ */
 export type TrimLeft<S extends string> = S extends `${WS}${infer L}`
   ? TrimLeft<L>
   : S
+
+/**
+ * Trim all whitespaces on the left side of a string
+ *
+ * @param S - String to trim
+ * @returns Trimmed string to the left side {@link S}
+ *
+ * @example
+ * type Result = TrimLeft<"    Hello">
+ * //    ^? "Hello"
+ */
 
 export type TrimRight<S extends string> = S extends `${infer R}${WS}`
   ? TrimRight<R>
   : S
 
+/**
+ * Trim all whitespaces on the right and left side of a string
+ *
+ * @param S - String to trim
+ * @returns Trimmed string from both right and left sides {@link S}
+ *
+ * @example
+ * type Result = Trim<"     Hello     ">
+ * //    ^? "Hello"
+ */
 export type Trim<S extends string> = TrimLeft<TrimRight<S>>
 
+// Convert uint string to proper representation of uint256
 export type SolidityType<T extends string> = T extends 'uint' ? 'uint256' : T
 
 export type AbiTypes = 'function' | 'event' | 'error'
@@ -17,18 +50,61 @@ export type AbiIndexed = ' indexed '
 
 export type AbiMutability = 'view' | 'pure' | 'payable' | 'nonpayable'
 
+/**
+ * Check if the a string has any "()". This is used after {@link ExtractArgs}
+ *
+ * @param T - String to check
+ * @returns True if it matches {@link T}
+ *
+ * @example
+ * type Result = hasTupleValue<"address owner, (uint tokenId)[] id">
+ * //    ^? "true"
+ */
 export type hasTupleValue<T extends string> =
   T extends `${string}(${string})${string}` ? true : false
 
+/**
+ * Check if the a string has any "()" at the start of the string. This is used after {@link ExtractArgs}
+ *
+ * @param T - String to check
+ * @returns True if it matches {@link T}
+ *
+ * @example
+ * type Result = isTupleValue<"(uint tokenId)[] id">
+ * //    ^? "true"
+ */
 export type isTupleValue<T extends string> = T extends `(${string})${string}`
   ? true
   : false
 
+/**
+ * Removes instance of a word from a given string
+ *
+ * @param T - Target string
+ * @param K - Value to remove
+ * @returns String without the {@link K} value in it {@link T}
+ *
+ * @example
+ * type Result = Remove<"HelloWorld", "World">
+ * //    ^? "Hello"
+ */
 export type Remove<
   T,
   K extends string,
 > = T extends `${infer Head}${K}${infer Tail}` ? `${Head}${Tail}` : T
 
+/**
+ * Replaces all instances of a word on a given string with the new values
+ *
+ * @param S - String to trim
+ * @param From - Word to remove
+ * @param To - Word to replace with
+ * @returns new string with all the replaced values {@link S}
+ *
+ * @example
+ * type Result = <"foobarbar", "bar", "foo">
+ * //    ^? "foofoofoo"
+ */
 export type ReplaceAll<
   S extends string,
   From extends string,
@@ -39,8 +115,28 @@ export type ReplaceAll<
   ? `${ReplaceAll<Prefix, From, To>}${To}${ReplaceAll<Suffix, From, To>}`
   : S
 
+/**
+ * Removes the last element of an array
+ *
+ * @param T - Any array value
+ * @returns Array without the last element of it {@link T}
+ *
+ * @example
+ * type Result = Pop<[1,2,3]>
+ * //    ^? "[1,2]"
+ */
 type Pop<T extends any[]> = T extends [...infer R, any] ? R : []
 
+/**
+ * Splits a string for every `,` value in it in the expection it has nested values e.g `(${string})`
+ *
+ * @param T - string to split
+ * @returns Array with the splited string {@link T}
+ *
+ * @example
+ * type Result = SplitNesting<'address owner, (bool loading, (string[][] names) cats)[] dog, uint tokenId'>
+ * //    ^? ["address owner","(bool loading, (string[][] names) cats)[] dog","uint tokenId"]
+ */
 export type SplitNesting<
   T extends string,
   TResult extends any[] = [],
@@ -64,6 +160,16 @@ export type SplitNesting<
     : SplitNesting<Rest, TResult, `${TStr}${Char}`, Depth>
   : []
 
+/**
+ * Re-order an array to ensure that strings that match `(${string})${string}` are the last element
+ *
+ * @param T - Array of strings
+ * @returns Order array according to the specs above {@link T}
+ *
+ * @example
+ * type Result = ReOrderArray<["address owner", "(bool loading, (string[][] names) cats)[] dog", "uint tokenId"]>
+ * //    ^? ["address owner","uint tokenId","(bool loading, (string[][] names) cats)[] dog"]
+ */
 export type ReOrderArray<
   T extends string[],
   TRemain extends any[] = [],
