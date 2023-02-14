f092720739a9fa896c70167637fb7cbd5567dc62
Tom Meagher <tom@meagher.co>
2023-02-14 17:16:45 -0500

    chore: checkpoint

--- src/human-readable/utils.test-d.ts
+++ src/human-readable/utils.test-d.ts
@@ -1,129 +1,229 @@
 import { assertType, test } from 'vitest'
 
-import type { ParseAbi, ParseAbiParameter, ParseAbiParameters } from './utils'
+import type { ParseAbiParameter, ParseParams, ParseSignature } from './utils'
+
+test('ParseSignature', () => {
+  assertType<ParseSignature<'function number((uint256 foo), string bar)'>>({
+    type: 'function',
+    name: 'number',
+    inputs: ['(uint256 foo)', 'string bar'],
+    outputs: [],
+    stateMutability: 'nonpayable',
+  })
+  assertType<
+    ParseSignature<'function number() returns ((uint256 foo), string bar)'>
+  >({
+    type: 'function',
+    name: 'number',
+    inputs: [],
+    outputs: ['(uint256 foo)', 'string bar'],
+  })
+  assertType<ParseSignature<'constructor((uint256 foo), string bar)'>>({
+    type: 'constructor',
+    inputs: ['(uint256 foo)', 'string bar'],
+  })
+  assertType<ParseSignature<'fallback()'>>({
+    type: 'fallback',
+  })
+  assertType<ParseSignature<'receive() external payable'>>({
+    type: 'receive',
+    stateMutability: 'payable',
+  })
+})
+
+test('ParseParams', () => {
+  assertType<
+    ParseParams<'address owner, (bool loading, (string[][] names) cats)[] dog, uint tokenId'>
+  >([
+    'address owner',
+    '(bool loading, (string[][] names) cats)[] dog',
+    'uint tokenId',
+  ])
+})
 
 test('ParseAbiParameter', () => {
+  assertType<ParseAbiParameter<'string'>>({ type: 'string' })
+  assertType<ParseAbiParameter<'(string foo)'>>({ type: '(string foo)' })
+  assertType<ParseAbiParameter<'(string foo)[]'>>({ type: '(string foo)[]' })
+  assertType<ParseAbiParameter<'(string foo)[10]'>>({
+    type: '(string foo)[10]',
+  })
+  assertType<ParseAbiParameter<'(string foo)[10][]'>>({
+    type: '(string foo)[10][]',
+  })
+  assertType<ParseAbiParameter<'(string foo)[][10]'>>({
+    type: '(string foo)[][10]',
+  })
+  assertType<ParseAbiParameter<'(string foo)[][10][]'>>({
+    type: '(string foo)[][10][]',
+  })
+  assertType<ParseAbiParameter<'((string foo))'>>({ type: '((string foo))' })
+  assertType<ParseAbiParameter<'(string foo, address baz)'>>({
+    type: '(string foo, address baz)',
+  })
+
   assertType<ParseAbiParameter<'string foo'>>({ type: 'string', name: 'foo' })
+  assertType<ParseAbiParameter<'(string bar) foo'>>({
+    type: '(string bar)',
+    name: 'foo',
+  })
+  assertType<ParseAbiParameter<'(string bar)[] foo'>>({
+    type: '(string bar)[]',
+    name: 'foo',
+  })
+  assertType<ParseAbiParameter<'((string bar)) foo'>>({
+    type: '((string bar))',
+    name: 'foo',
+  })
+  assertType<ParseAbiParameter<'(string bar, address baz) foo'>>({
+    type: '(string bar, address baz)',
+    name: 'foo',
+  })
+
   assertType<ParseAbiParameter<'string indexed foo'>>({
     type: 'string',
     name: 'foo',
     indexed: true,
   })
-  assertType<ParseAbiParameter<'string indexed'>>({
-    type: 'string',
-    indexed: true,
-    name: '',
-  })
-  assertType<ParseAbiParameter<'string'>>({ type: 'string', name: '' })
-
-  assertType<ParseAbiParameter<'string foo bar baz'>>({
-    type: 'string',
-    name: 'Error: "foo bar baz" contains whitespace"',
-  })
-  assertType<ParseAbiParameter<'string indexed foo bar baz'>>({
-    type: 'string',
-    indexed: true,
-    name: 'Error: "foo bar baz" contains whitespace"',
-  })
-
-  assertType<
-    ParseAbiParameter<
-      'Name foo',
-      {
-        Name: [{ type: 'string'; name: 'bar' }]
-      }
-    >
-  >({
-    type: 'tuple',
+  assertType<ParseAbiParameter<'(string bar) indexed foo'>>({
+    type: '(string bar)',
     name: 'foo',
-    components: [{ type: 'string', name: 'bar' }],
-    internalType: 'struct Name',
+    indexed: true,
+  })
+  assertType<ParseAbiParameter<'((string bar)) indexed foo'>>({
+    type: '((string bar))',
+    name: 'foo',
+    indexed: true,
+  })
+  assertType<ParseAbiParameter<'(string bar, address baz) indexed foo'>>({
+    type: '(string bar, address baz)',
+    name: 'foo',
+    indexed: true,
   })
 })
 
-test('ParseAbiParameters', () => {
-  assertType<ParseAbiParameters<'string foo'>>([
-    { type: 'string', name: 'foo' },
-  ])
-  assertType<ParseAbiParameters<'string foo, string bar'>>([
-    { type: 'string', name: 'foo' },
-    { type: 'string', name: 'bar' },
-  ])
-  assertType<
-    ParseAbiParameters<
-      'string foo, Bar bar',
-      {
-        Bar: [{ type: 'string'; name: 'foo' }, { type: 'string'; name: 'bar' }]
-      }
-    >
-  >([
-    { type: 'string', name: 'foo' },
-    {
-      type: 'tuple',
-      name: 'bar',
-      components: [
-        { type: 'string', name: 'foo' },
-        { type: 'string', name: 'bar' },
-      ],
-    },
-  ])
-  assertType<ParseAbiParameters<'string foo, Bar bar'>>([
-    { type: 'string', name: 'foo' },
-    {
-      type: 'Error: Unknown type "Bar"',
-      name: 'bar',
-    },
-  ])
-})
+// test('ParseAbiParameter', () => {
+//   assertType<ParseAbiParameter<'string foo'>>({ type: 'string', name: 'foo' })
+//   assertType<ParseAbiParameter<'string indexed foo'>>({
+//     type: 'string',
+//     name: 'foo',
+//     indexed: true,
+//   })
+//   assertType<ParseAbiParameter<'string indexed'>>({
+//     type: 'string',
+//     indexed: true,
+//     name: '',
+//   })
+//   assertType<ParseAbiParameter<'string'>>({ type: 'string', name: '' })
 
-test('ParseAbi', () => {
-  type Result = ParseAbi<
-    // ^?
-    [
-      'struct Name { string first; string last; }',
-      'struct Person { Name name; uint16 age; }',
-      'function number() returns (uint256)',
-      'function setNumber(uint256 value)',
-      'function addPerson(Person person)',
-    ]
-  >
-  assertType<Result>([
-    {
-      type: 'function',
-      name: 'number',
-      inputs: [],
-      outputs: [{ type: 'uint256', name: '' }],
-      stateMutability: 'nonpayable',
-    },
-    {
-      type: 'function',
-      name: 'setNumber',
-      inputs: [{ type: 'uint256', name: 'value' }],
-      outputs: [],
-      stateMutability: 'nonpayable',
-    },
-    {
-      type: 'function',
-      name: 'addPerson',
-      inputs: [
-        {
-          type: 'tuple',
-          name: 'person',
-          components: [
-            {
-              type: 'tuple',
-              name: 'name',
-              components: [
-                { type: 'string', name: 'first' },
-                { type: 'string', name: 'last' },
-              ],
-            },
-            { type: 'uint16', name: 'age' },
-          ],
-        },
-      ],
-      outputs: [],
-      stateMutability: 'nonpayable',
-    },
-  ])
-})
+//   assertType<ParseAbiParameter<'string foo bar baz'>>({
+//     type: 'string',
+//     name: 'Error: "foo bar baz" contains whitespace"',
+//   })
+//   assertType<ParseAbiParameter<'string indexed foo bar baz'>>({
+//     type: 'string',
+//     indexed: true,
+//     name: 'Error: "foo bar baz" contains whitespace"',
+//   })
+
+//   assertType<
+//     ParseAbiParameter<
+//       'Name foo',
+//       {
+//         Name: [{ type: 'string'; name: 'bar' }]
+//       }
+//     >
+//   >({
+//     type: 'tuple',
+//     name: 'foo',
+//     components: [{ type: 'string', name: 'bar' }],
+//     internalType: 'struct Name',
+//   })
+// })
+
+// test('ParseAbiParameters', () => {
+//   assertType<ParseAbiParameters<'string foo'>>([
+//     { type: 'string', name: 'foo' },
+//   ])
+//   assertType<ParseAbiParameters<'string foo, string bar'>>([
+//     { type: 'string', name: 'foo' },
+//     { type: 'string', name: 'bar' },
+//   ])
+//   assertType<
+//     ParseAbiParameters<
+//       'string foo, Bar bar',
+//       {
+//         Bar: [{ type: 'string'; name: 'foo' }, { type: 'string'; name: 'bar' }]
+//       }
+//     >
+//   >([
+//     { type: 'string', name: 'foo' },
+//     {
+//       type: 'tuple',
+//       name: 'bar',
+//       components: [
+//         { type: 'string', name: 'foo' },
+//         { type: 'string', name: 'bar' },
+//       ],
+//     },
+//   ])
+//   assertType<ParseAbiParameters<'string foo, Bar bar'>>([
+//     { type: 'string', name: 'foo' },
+//     {
+//       type: 'Error: Unknown type "Bar"',
+//       name: 'bar',
+//     },
+//   ])
+// })
+
+// test('ParseAbi', () => {
+//   type Result = ParseAbi<
+//     // ^?
+//     [
+//       'struct Name { string first; string last; }',
+//       'struct Person { Name name; uint16 age; }',
+//       'function number() returns (uint256)',
+//       'function setNumber(uint256 value)',
+//       'function addPerson(Person person)',
+//     ]
+//   >
+//   assertType<Result>([
+//     {
+//       type: 'function',
+//       name: 'number',
+//       inputs: [],
+//       outputs: [{ type: 'uint256', name: '' }],
+//       stateMutability: 'nonpayable',
+//     },
+//     {
+//       type: 'function',
+//       name: 'setNumber',
+//       inputs: [{ type: 'uint256', name: 'value' }],
+//       outputs: [],
+//       stateMutability: 'nonpayable',
+//     },
+//     {
+//       type: 'function',
+//       name: 'addPerson',
+//       inputs: [
+//         {
+//           type: 'tuple',
+//           name: 'person',
+//           components: [
+//             {
+//               type: 'tuple',
+//               name: 'name',
+//               components: [
+//                 { type: 'string', name: 'first' },
+//                 { type: 'string', name: 'last' },
+//               ],
+//             },
+//             { type: 'uint16', name: 'age' },
+//           ],
+//         },
+//       ],
+//       outputs: [],
+//       stateMutability: 'nonpayable',
+//     },
+//   ])
+// })
--- src/human-readable/utils.ts
+++ src/human-readable/utils.ts
@@ -1,69 +1,174 @@
-import type { AbiParameter, AbiType } from '../abi'
-import type { AssertNoWhiteSpace, Filter, Split, Trim } from '../types'
-import type { ParseFunction } from './functions'
-import type { IsFunctionSignature, Signature } from './signatures'
-import type { ParseStructs, StructMap } from './structs'
+import type { AbiType, SolidityArrayWithTuple, SolidityTuple } from '../abi'
+import type { Trim } from '../types'
+import type {
+  ConstructorSignature,
+  FunctionSignatureWithReturn,
+  FunctionSignatureWithoutReturn,
+  IsConstructorSignature,
+  IsFallbackSignature,
+  IsFunctionSignature,
+  IsReceiveSignature,
+} from './signatures'
 
-export type ParseAbiParameter<
-  T extends string,
-  TStructs extends Record<string, readonly AbiParameter[]> | unknown = unknown,
-  Options extends { throwUnknownType: boolean } = { throwUnknownType: true },
-> = (
-  T extends `${infer Type} indexed${infer Name}`
-    ? {
-        name: AssertNoWhiteSpace<Name>
-        type: Type
-        indexed: true
-      }
-    : T extends `${infer Type} ${infer Name}`
-    ? { name: AssertNoWhiteSpace<Name>; type: Type }
-    : { name: ''; type: T }
-) extends infer Parameter extends AbiParameter
-  ? Parameter['type'] extends AbiType
-    ? Parameter
-    : Parameter['type'] extends `${infer Head extends string &
-        keyof TStructs}[${infer Tail}]`
-    ? {
-        name: Parameter['name']
-        type: `tuple[${Tail}]`
-        components: TStructs[Head]
-        internalType?: `struct ${Parameter['type']}`
-      }
-    : Parameter['type'] extends keyof TStructs
-    ? {
-        name: Parameter['name']
-        type: 'tuple'
-        components: TStructs[Parameter['type']]
-        internalType?: `struct ${Parameter['type']}`
-      }
-    : Options['throwUnknownType'] extends true
-    ? {
-        name: Parameter['name']
-        type: `Error: Unknown type "${Parameter['type']}"`
-      }
-    : Parameter
-  : never
+// 1. Get params from signature
+// 2. Parse params into array
+// 3. Convert each param string to basic abi parameter
+// 4. Resolve types for each param to create full param (if inline tuple repeat steps 1-4)
 
-export type ParseAbiParameters<
+export type ParseSignature<TSignature extends string> =
+  | (IsFunctionSignature<TSignature> extends true
+      ? TSignature extends FunctionSignatureWithReturn<
+          infer Name,
+          infer Params,
+          infer Return
+        >
+        ? {
+            name: Trim<Name>
+            type: 'function'
+            inputs: Trim<Params> extends '' ? [] : ParseParams<Trim<Params>>
+            outputs: Return extends `${string}returns (${infer Outputs})`
+              ? ParseParams<Trim<Outputs>>
+              : []
+          }
+        : TSignature extends FunctionSignatureWithoutReturn<
+            infer Name,
+            infer Params
+          >
+        ? {
+            name: Trim<Name>
+            type: 'function'
+            stateMutability: 'nonpayable'
+            inputs: ParseParams<Trim<Params>>
+            outputs: []
+          }
+        : never
+      : never)
+  | (IsConstructorSignature<TSignature> extends true
+      ? {
+          type: 'constructor'
+          inputs: TSignature extends ConstructorSignature<infer Params>
+            ? ParseParams<Trim<Params>>
+            : []
+        }
+      : never)
+  | (IsFallbackSignature<TSignature> extends true
+      ? {
+          type: 'fallback'
+        }
+      : never)
+  | (IsReceiveSignature<TSignature> extends true
+      ? {
+          type: 'receive'
+          stateMutability: 'payable'
+        }
+      : never)
+
+export type ParseParams<
   T extends string,
-  TStructs extends StructMap | unknown = unknown,
+  Result extends unknown[] = [],
+  Current extends string = '',
+  Depth extends ReadonlyArray<number> = [],
 > = T extends ''
-  ? []
-  : Split<T, ','> extends infer Inputs extends string[]
-  ? {
-      [K in keyof Inputs]: ParseAbiParameter<Trim<Inputs[K]>, TStructs>
-    }
+  ? [...Result, Trim<Current>]
+  : Depth['length'] extends 0
+  ? T extends `${infer Head}${infer Tail}`
+    ? Head extends ','
+      ? ParseParams<Tail, [...Result, Trim<Current>], ''>
+      : Head extends '('
+      ? ParseParams<Tail, Result, `${Current}${Head}`, [...Depth, 1]>
+      : ParseParams<Tail, Result, `${Current}${Head}`>
+    : []
+  : T extends `${infer Char}${infer Rest}`
+  ? Char extends '('
+    ? ParseParams<Rest, Result, `${Current}${Char}`, [...Depth, 1]>
+    : Char extends ')'
+    ? ParseParams<Rest, Result, `${Current}${Char}`, Pop<Depth>>
+    : ParseParams<Rest, Result, `${Current}${Char}`, Depth>
   : []
+type Pop<T extends ReadonlyArray<number>> = T extends [...infer R, any] ? R : []
 
-export type ParseAbi<TSignatures extends readonly Signature[]> =
-  ParseStructs<TSignatures> extends infer Structs
-    ? {
-        [K in keyof TSignatures]: TSignatures[K] extends infer Signature extends string
-          ? IsFunctionSignature<Signature> extends true
-            ? ParseFunction<Signature, Structs>
-            : never
-          : never
-      } extends infer Abi extends readonly unknown[]
-      ? Filter<Abi, never>
-      : []
+export type ParseAbiParameter<T extends string> =
+  T extends `(${string})${string}`
+    ? ParseComponents<[T]>
+    : T extends `${infer Type} indexed ${infer Name}`
+    ? { type: Trim<Type>; name: Trim<Name>; indexed: true }
+    : T extends `${infer Type} ${infer Name}`
+    ? { type: Trim<Type>; name: Trim<Name> }
+    : T extends `${infer Type}`
+    ? { type: Type }
     : never
+
+type Result = ParseAbiParameter<'(string)[][] foo'>
+
+type ParseAbiParameters<T extends readonly string[]> = {
+  [K in keyof T]: ParseAbiParameter<T[K]>
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+type UnnamedArgs = Exclude<AbiType, SolidityTuple | SolidityArrayWithTuple>
+type Modifier = 'calldata' | 'indexed' | 'memory' | 'storage'
+type AbiArgs =
+  | UnnamedArgs
+  | ''
+  | 'void'
+  | `${string}${Modifier}${string}`
+  | `${string} ${string}`
+type TupleValue = `(${string})${string}`
+type AbiArgsWithTuple = AbiArgs | TupleValue
+type AbiArgsTypeWithTuple = readonly AbiArgsWithTuple[]
+type IsIndexed<T extends string> = T extends `${string} indexed ${string}`
+  ? true
+  : unknown
+type ExtractTArgs<T extends string> =
+  T extends `(${infer Args})${ExtractTupleInfo<T>}`
+    ? Args
+    : T extends `(${infer Args}) ${ExtractTupleInfo<T>}`
+    ? Args
+    : ''
+// type Result = ExtractTArgs<'(string bar, address foo)[] foo'>
+type ExtractTupleInfo<T extends string> = T extends `${string})${infer Info}`
+  ? Info extends `${string})`
+    ? ''
+    : Info extends `${string})${infer TInfo}`
+    ? TInfo extends ''
+      ? Trim<Info>
+      : ExtractTupleInfo<Trim<TInfo>>
+    : Trim<Info>
+  : T
+type ExtractTupleType<T extends string> = T extends `[${infer K}]${string}`
+  ? `tuple[${K}]`
+  : 'tuple'
+type ExtractTupleName<T extends string> =
+  T extends `[${string}] indexed ${infer Name}`
+    ? Name
+    : T extends `[${string}]${infer Name}`
+    ? Trim<Name>
+    : T extends `indexed ${infer Name}`
+    ? Name
+    : T
+
+type ParseComponents<T extends AbiArgsTypeWithTuple> = T extends [never]
+  ? never
+  : T extends ['']
+  ? never
+  : T extends [
+      infer Head extends AbiArgsWithTuple,
+      ...infer Last extends AbiArgsTypeWithTuple,
+    ]
+  ? Head extends `(${string}`
+    ? [
+        {
+          type: ExtractTupleType<ExtractTupleInfo<Head>>
+          name: ExtractTupleName<ExtractTupleInfo<Head>>
+          // internalType: ExtractTupleInternalType<ExtractTupleInfo<Head>>
+          components: [
+            ...ParseComponents<[...ParseParams<ExtractTArgs<Head>>, ...Last]>,
+          ]
+        } & (IsIndexed<Head> extends true ? { indexed: true } : unknown),
+      ]
+    : [
+        ...ParseAbiParameters<[Exclude<Head, TupleValue>]>,
+        ...ParseComponents<Last>,
+      ]
+  : []
