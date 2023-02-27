4c4aef24df1f7799b63a56b4371d5d52c39996d5
Raiden1411 <joao9_bernardino@hotmail.com>
2023-02-27 21:40:48 +0000

    wip: runtime parseHumanAbiFallbacks

--- src/human-abi/parseMethods/parseHumanAbiFallbacks.ts
+++ src/human-abi/parseMethods/parseHumanAbiFallbacks.ts
@@ -1,8 +1,40 @@
 import type { Narrow } from '../../narrow'
 import type { HAbi, ParseHAbiFallbacks } from '../habi'
+import type { FallbacksSignature } from '../utils'
+import { fallbackRegex, receiveRegex } from './regex'
 
 export function parseHumanAbiFallbacks<THAbi extends HAbi>(
   signatures: Narrow<THAbi>,
 ): ParseHAbiFallbacks<THAbi> {
-  return signatures as ParseHAbiFallbacks<THAbi>
+  const fallbackSignatures = signatures.filter(
+    (val) => fallbackRegex.test(val) || receiveRegex.test(val),
+  )
+
+  if (fallbackSignatures.length === 0) return [] as ParseHAbiFallbacks<THAbi>
+
+  const abiObjects = []
+
+  for (let i = 0; i < fallbackSignatures.length; i++) {
+    abiObjects.push(
+      parseHumanAbiFallback(fallbackSignatures[i] as FallbacksSignature),
+    )
+  }
+
+  return abiObjects as ParseHAbiFallbacks<THAbi>
+}
+
+export function parseHumanAbiFallback(signature: string) {
+  if (fallbackRegex.test(signature))
+    return {
+      type: 'fallback',
+    }
+
+  if (receiveRegex.test(signature)) {
+    return {
+      type: 'receive',
+      stateMutability: 'payable',
+    }
+  }
+
+  throw new Error(`Invalid signature: ${signature}`)
 }
