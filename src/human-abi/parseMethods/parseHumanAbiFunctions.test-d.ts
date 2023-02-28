e46e8e2d1a65f8605cc8ab682f7a2cd020d95722
Raiden1411 <joao9_bernardino@hotmail.com>
2023-02-28 18:40:30 +0000

    wip: parseHumanAbiFunctions

--- src/human-abi/parseMethods/parseHumanAbiFunctions.test-d.ts
+++ src/human-abi/parseMethods/parseHumanAbiFunctions.test-d.ts
@@ -2,24 +2,35 @@ import { expectTypeOf, test } from 'vitest'
 
 import type { DeepReadonly } from '../utils'
 import { parseHumanAbiFunctions } from './parseHumanAbiFunctions'
+import { createStructObject } from './utils'
 
 test('Parse Human Abi Functions', () => {
+  const abi = [
+    'struct Color { uint8 red; uint8 green; uint8 blue; }',
+    'struct Foo { string name; Color color; }',
+    'function foo(Foo bar) view returns (string)',
+    'event Foo(Foo bar)',
+    'constructor(string symbol, string name)',
+    'function transferFrom(address from, address to, uint value)',
+    'function balanceOf(address owner) view returns (uint256 balance)',
+    'event Transfer(address indexed from, address indexed to, address value)',
+    'error InsufficientBalance(account owner, uint balance)',
+    'function addPerson((string name, uint16 age) person)',
+    'function addPeople((string name, uint16 age)[] person)',
+    'function getPerson(uint id) view returns ((string name, uint16 age))',
+    'event PersonAdded(uint indexed id, (string name, uint16 age) person)',
+  ] as const
+
+  const structs = [
+    'struct Color { uint8 red; uint8 green; uint8 blue; }',
+    'struct Foo { string name; Color color; }',
+  ] as const
   expectTypeOf(
-    parseHumanAbiFunctions([
-      'struct Color { uint8 red; uint8 green; uint8 blue; }',
-      'struct Foo { string name; Color color; }',
-      'function foo(Foo bar) view returns (string)',
-      'event Foo(Foo bar)',
-      'constructor(string symbol, string name)',
-      'function transferFrom(address from, address to, uint value)',
-      'function balanceOf(address owner) view returns (uint256 balance)',
-      'event Transfer(address indexed from, address indexed to, address value)',
-      'error InsufficientBalance(account owner, uint balance)',
-      'function addPerson((string name, uint16 age) person)',
-      'function addPeople((string name, uint16 age)[] person)',
-      'function getPerson(uint id) view returns ((string name, uint16 age))',
-      'event PersonAdded(uint indexed id, (string name, uint16 age) person)',
-    ]),
+    parseHumanAbiFunctions(abi, {
+      parseContext: 'function',
+      structTypes: new WeakSet(),
+      structs: createStructObject(structs),
+    }),
   ).toEqualTypeOf<
     DeepReadonly<
       [
--- src/human-abi/parseMethods/parseHumanAbiFunctions.ts
+++ src/human-abi/parseMethods/parseHumanAbiFunctions.ts
@@ -1,8 +1,62 @@
+import type { AbiFunction } from '../../abi'
 import type { Narrow } from '../../narrow'
 import type { HAbi, ParseHAbiFunctions } from '../habi'
+import type { AbiMutability, FunctionSignature } from '../utils'
+import { parseHumanAbiArguments } from './parseHumanAbiArguments'
+import type { Context } from './parseHumanAbiArguments'
+import { functionRegex } from './regex'
+import { parseParameters } from './utils'
 
-export function parseHumanAbiFunctions<THAbi extends HAbi>(
-  signatures: Narrow<THAbi>,
-): ParseHAbiFunctions<THAbi> {
-  return signatures as ParseHAbiFunctions<THAbi>
+export function parseHumanAbiFunctions<
+  THAbi extends HAbi,
+  TContext extends Context,
+>(signatures: Narrow<THAbi>, context: TContext): ParseHAbiFunctions<THAbi> {
+  const filtered = signatures.filter((val) => functionRegex.test(val))
+
+  const result = []
+
+  if (filtered.length === 0) {
+    return filtered as ParseHAbiFunctions<THAbi>
+  }
+
+  for (let i = 0; i < filtered.length; i++) {
+    const extracted = functionRegex.exec(filtered[i] as FunctionSignature)
+
+    if (!extracted)
+      throw new Error(`Error: Invalid signature provided '${filtered[i]}'`)
+    const { name, parameters, mutability, returnParameters } = extracted.groups
+
+    result.push({
+      type: 'function',
+      name: name ?? '',
+      stateMutability: (mutability as AbiMutability) ?? 'nonpayable',
+      inputs: parseHumanAbiArguments(parseParameters(parameters), context),
+      outputs: parseHumanAbiArguments(
+        parseParameters(returnParameters),
+        context,
+      ),
+    })
+  }
+
+  return result as ParseHAbiFunctions<THAbi>
+}
+
+export function parseHumanAbiFunction<
+  THAbiSignature extends string,
+  TContext extends Context,
+>(signature: THAbiSignature, context: TContext): AbiFunction {
+  const extracted = functionRegex.exec(signature)
+
+  if (!extracted)
+    throw new Error(`Error: Invalid signature provided '${signature}'`)
+
+  const { name, parameters, mutability, returnParameters } = extracted.groups
+
+  return {
+    type: 'function',
+    name: name ?? '',
+    stateMutability: (mutability as AbiMutability) ?? 'nonpayable',
+    inputs: parseHumanAbiArguments(parseParameters(parameters), context),
+    outputs: parseHumanAbiArguments(parseParameters(returnParameters), context),
+  }
 }
