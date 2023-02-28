dd22216ea8bc315ba72373b9ff877ccb0a82588f
Raiden1411 <joao9_bernardino@hotmail.com>
2023-02-28 18:41:59 +0000

    wip: runtime parseHumanAbiSignature and parseHumanAbi

--- src/human-abi/parseMethods/parseHumanAbi.ts
+++ src/human-abi/parseMethods/parseHumanAbi.ts
@@ -1,8 +1,41 @@
 import type { Narrow } from '../../narrow'
 import type { HAbi, ParseHumanAbi } from '../habi'
+import type { StructSignature } from '../utils'
+import { parseHumanAbiConstructors } from './parseHumanAbiConstructor'
+import { parseHumanAbiErrors } from './parseHumanAbiErrors'
+import { parseHumanAbiEvents } from './parseHumanAbiEvents'
+import { parseHumanAbiFallbacks } from './parseHumanAbiFallbacks'
+import { parseHumanAbiFunctions } from './parseHumanAbiFunctions'
+import { structRegex } from './regex'
+import { createStructObject } from './utils'
 
 export function parseHumanAbi<THAbi extends HAbi>(
   signatures: Narrow<THAbi>,
 ): ParseHumanAbi<THAbi> {
-  return signatures as ParseHumanAbi<THAbi>
+  const filtered: string[] = signatures.filter((val: string) =>
+    structRegex.test(val),
+  )
+
+  const structObject = createStructObject(filtered as StructSignature[])
+  const result = [
+    ...parseHumanAbiConstructors(signatures),
+    ...parseHumanAbiErrors(signatures, {
+      structs: structObject,
+      structTypes: new WeakSet(),
+      parseContext: 'error',
+    }),
+    ...parseHumanAbiEvents(signatures, {
+      structs: structObject,
+      structTypes: new WeakSet(),
+      parseContext: 'event',
+    }),
+    ...parseHumanAbiFunctions(signatures, {
+      structs: structObject,
+      structTypes: new WeakSet(),
+      parseContext: 'function',
+    }),
+    ...parseHumanAbiFallbacks(signatures),
+  ]
+
+  return result as unknown as ParseHumanAbi<THAbi>
 }
