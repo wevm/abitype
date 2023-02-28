38713dfd9b4e18e7232fedc5ed8c62b8de38ddac
Raiden1411 <joao9_bernardino@hotmail.com>
2023-02-28 18:38:52 +0000

    wip: runtime parseHumanAbiErrors

--- src/human-abi/parseMethods/parseHumanAbiErrors.test-d.ts
+++ src/human-abi/parseMethods/parseHumanAbiErrors.test-d.ts
@@ -4,22 +4,26 @@ import type { DeepReadonly } from '../utils'
 import { parseHumanAbiErrors } from './parseHumanAbiErrors'
 
 test('Parse Human Abi', () => {
+  const abi = [
+    'struct Color { uint8 red; uint8 green; uint8 blue; }',
+    'struct Foo { string name; Color color; }',
+    'function foo(Foo bar) view returns (string)',
+    'event Foo(Foo bar)',
+    'constructor(string symbol, string name)',
+    'function transferFrom(address from, address to, uint value)',
+    'function balanceOf(address owner) view returns (uint256 balance)',
+    'event Transfer(address indexed from, address indexed to, address value)',
+    'error InsufficientBalance(account owner, uint balance)',
+    'function addPerson((string name, uint16 age) person)',
+    'function addPeople((string name, uint16 age)[] person)',
+    'function getPerson(uint id) view returns ((string name, uint16 age))',
+    'event PersonAdded(uint indexed id, (string name, uint16 age) person)',
+  ] as const
   expectTypeOf(
-    parseHumanAbiErrors([
-      'struct Color { uint8 red; uint8 green; uint8 blue; }',
-      'struct Foo { string name; Color color; }',
-      'function foo(Foo bar) view returns (string)',
-      'event Foo(Foo bar)',
-      'constructor(string symbol, string name)',
-      'function transferFrom(address from, address to, uint value)',
-      'function balanceOf(address owner) view returns (uint256 balance)',
-      'event Transfer(address indexed from, address indexed to, address value)',
-      'error InsufficientBalance(account owner, uint balance)',
-      'function addPerson((string name, uint16 age) person)',
-      'function addPeople((string name, uint16 age)[] person)',
-      'function getPerson(uint id) view returns ((string name, uint16 age))',
-      'event PersonAdded(uint indexed id, (string name, uint16 age) person)',
-    ]),
+    parseHumanAbiErrors(abi, {
+      parseContext: 'event',
+      structTypes: new WeakSet(),
+    }),
   ).toEqualTypeOf<
     DeepReadonly<
       [
