1a0f3d73b551c3088b4d8366788f004d80d13181
0xRaiden <67233402+Raiden1411@users.noreply.github.com>
2023-04-29 22:32:53 +0100

    fix: rename export

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -5,7 +5,7 @@ import * as Exports from './'
 it('should expose correct exports', () => {
   expect(Object.keys(Exports)).toMatchInlineSnapshot(`
     [
-      "address",
+      "zeroAddress",
       "customSolidityErrorsAbi",
       "ensAbi",
       "ensRegistryWithFallbackAbi",
