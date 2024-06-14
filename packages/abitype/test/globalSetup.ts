696b2ab88ee857d3f0200d9fbf1675cf783369be
awkweb <tom@meagher.co>
2024-06-14 13:06:53 -0400

    chore: bump attest (#237)
    * chore: bump attest
    
    * chore: add join
    
    * chore: up
    
    * chore: bump attest
    
    * chore: deps
    
    * chore: format
    
    * chore: change build module
    
    * docs: up casing
    
    ---------
    
    Co-authored-by: tmm <tmm@users.noreply.github.com>

--- packages/abitype/test/globalSetup.ts
+++ packages/abitype/test/globalSetup.ts
@@ -1,6 +1,17 @@
-import { cleanup, setup } from '@arktype/attest'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { setup } from '@arktype/attest'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default function () {
-  setup()
-  return cleanup
+  return setup({
+    benchErrorOnThresholdExceeded: true,
+    benchPercentThreshold: 10,
+    tsconfig: resolve(__dirname, '../tsconfig.json'),
+    formatter: 'pnpm biome format --write',
+  })
 }
+
+export { teardown } from '@arktype/attest'
