91f684d3150fea077e271d829f462d9b509fc946
Tom Meagher <tom@meagher.co>
2024-12-04 17:29:46 -0500

    ci: pr previews

--- scripts/updateVersion.ts
+++ scripts/updateVersion.ts
@@ -1,3 +1,4 @@
+import { execSync } from 'node:child_process'
 import path from 'node:path'
 import { glob } from 'glob'
 
@@ -23,22 +24,31 @@ for (const packagePath of packagePaths) {
   // Skip private packages
   if (packageJson.private) continue
 
+  const version = (() => {
+    if (Bun.env.PKG_PR_NEW) {
+      const gitHash = execSync('git rev-parse --short HEAD').toString().trim()
+      const branch = execSync('git branch --show-current')
+        .toString()
+        .trim()
+        .replace(/[^a-zA-Z0-9]/g, '_')
+      return `0.0.0-${branch}.${gitHash}`
+    }
+    return packageJson.version
+  })()
+
   count += 1
-  console.log(`${packageJson.name} — ${packageJson.version}`)
+  console.log(`${packageJson.name} — ${version}`)
 
   const versionFilePath = path.resolve(
     path.dirname(packagePath),
     'src',
     'version.ts',
   )
-  await Bun.write(
-    versionFilePath,
-    `export const version = '${packageJson.version}'\n`,
-  )
+  await Bun.write(versionFilePath, `export const version = '${version}'\n`)
 
   const jsrFilePath = path.resolve(path.dirname(packagePath), 'jsr.json')
   const jsrJson = await Bun.file(jsrFilePath).json()
-  jsrJson.version = packageJson.version
+  jsrJson.version = version
   Bun.write(jsrFilePath, JSON.stringify(jsrJson, null, 2))
 }
 
